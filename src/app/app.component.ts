3754d3c12410016444a28c7d56c7184b923c76c6
Aquariuslt <superaquariuslt@gmail.com>
2017-03-18 15:07:24 +0800

    [SETUP] angular-cli structure initial

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-root',
+  styles: [],
+  template: `
+    <h1>App Works</h1>
+  `,
+})
+export class AppComponent {
+  title = 'Aexo works';
+}
